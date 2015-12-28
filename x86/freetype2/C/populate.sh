3f888f296ce7af3aae4bf6ca5b9c7a9311cbc1b0
Jeffrey Massung <massung@gmail.com>
2015-12-28 16:02:37 -0600

    Added freetype2 headers.

---
+++ x86/freetype2/C/populate.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+h-to-ffi.sh -isystem /usr/include/freetype2 /usr/include/freetype2/ft2build.h
---
+++ x86_64/freetype2/C/populate.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+h-to-ffi.sh -isystem /usr/include/freetype2 -m64 /usr/include/freetype2/ft2build.h
