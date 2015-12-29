e3976014d06d0b87057022745de1687b1b23ce73
Jeffrey Massung <massung@gmail.com>
2015-12-29 08:36:31 -0600

    Couldn't get Freetype2 to build properly. Removing.

--- x86/freetype2/C/populate.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-h-to-ffi.sh -isystem /usr/include/freetype2 /usr/include/freetype2/ft2build.h
--- x86_64/freetype2/C/populate.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-h-to-ffi.sh -isystem /usr/include/freetype2 -m64 /usr/include/freetype2/ft2build.h
